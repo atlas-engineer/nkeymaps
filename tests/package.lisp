1cb6d181ec4b39c2e75d083d3a6bb7b6cf74f113
Andre A. Gomes <andremegafone@gmail.com>
2023-06-16 16:56:17 +0300

    tests: Adapt to new keyspec styles.

--- tests/package.lisp
+++ tests/package.lisp
@@ -6,3 +6,10 @@
 (uiop:define-package :nkeymaps/tests
   (:use :common-lisp :lisp-unit2)
   (:import-from :nkeymaps))
+
+(in-package :nkeymaps/tests)
+
+(defun cua-modifiers (body-fn)
+  "A context that locally binds `nkeymaps:*print-shortcut*' to \"cua\"."
+  (let ((nkeymaps:*print-shortcut* "cua"))
+    (funcall body-fn)))
