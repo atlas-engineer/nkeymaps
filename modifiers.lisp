5f204613aedfea7c886ceba1144205c67a1c25b3
John Mercouris <john@mercouris.email>
2023-09-14 13:24:47 -0400

    modifiers.lisp: Redo reader macros for clarity.

--- modifiers.lisp
+++ modifiers.lisp
@@ -5,19 +5,28 @@
 
 (defparameter +control+
   (define-modifier :string "control" :shortcut "C"
-    :system-name #+darwin "control" #-darwin "Ctrl")
+    :system-name
+    #+darwin "control"
+    #-darwin "Ctrl")
   "The key modifier commonly known as 'control'.")
 (defparameter +meta+
   (define-modifier :string "meta" :shortcut "M"
-    :system-name #+darwin "option" #-darwin "Alt")
+    :system-name
+    #+darwin "option"
+    #-darwin "Alt")
   "The key modifier commonly known as 'alt', 'option' or 'meta'.")
 (defparameter +shift+
   (define-modifier :string "shift" :shortcut "s"
-    :system-name #+darwin "shift" #-darwin "Shift")
+    :system-name
+    #+darwin "shift"
+    #-darwin "Shift")
   "The key modifier commonly known as 'shift'.")
 (defparameter +super+
   (define-modifier :string "super" :shortcut "S"
-    :system-name #+darwin "command" #+win32 "Win" #+linux "Super")
+    :system-name
+    #+darwin "command"
+    #+win32 "Win"
+    #+linux "Super")
   "The key modifier commonly known as 'windows', 'command' or 'super'.")
 (defparameter +hyper+
   (define-modifier :string "hyper" :shortcut "H"
